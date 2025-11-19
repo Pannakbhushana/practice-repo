3953a07aefd219aad131ae08f64e1c54420474e6
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2025-11-19 12:05:28 +0530

    new vite project added

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <h1 className='text-8xl font-bold text-blue-900'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App

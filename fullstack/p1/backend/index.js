333fe09e18393527ee20c64eaac39bbec7689369
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2025-11-29 10:47:18 +0530

    logger middleware commented out

--- fullstack/p1/backend/index.js
+++ fullstack/p1/backend/index.js
@@ -3,13 +3,13 @@ const app = express();
 const {connection} = require("./db")
 const cors = require("cors");
 const {postRouter} = require("./routes/post.route")
-const {logger} = require("./middlewares/logger.middleware");
+// const {logger} = require("./middlewares/logger.middleware");
 const {authRouter} = require("./routes/auth.route");
 const {verifyToken} = require("./middlewares/verifytoken.middleware");
 
 app.use(express.json());
 app.use(cors())
-app.use(logger);
+// app.use(logger);
 app.use("/auth", authRouter)
 app.use("/post", verifyToken, postRouter)
 

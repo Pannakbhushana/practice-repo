7cab0efd4c523d6c89e3bb367d7e60253a120294
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2025-12-18 11:16:26 +0530

    just practicing

--- venila-javascript/debounce-and-throtling/practice.js
+++ venila-javascript/debounce-and-throtling/practice.js
@@ -1,46 +1,47 @@
-const debounceOutPut = document.getElementById("debounceOutput");
 const debounceInput = document.getElementById("debounceInput");
+const debounceOutput = document.getElementById("debounceOutput");
 
 const debounce = (fn, delay) => {
     let timerId;
 
-    return function(...args){
+    return function (...args) {
         clearTimeout(timerId);
-
         timerId = setTimeout(()=>{
-            fn.apply(this, args);
+            fn(...args)
         },delay)
     }
 }
-
-const handleDebounce = debounce(function(event){
-    debounceOutPut.textContent = event.target.value;
+const handleDebounce = debounce(function(event, name){
+    debounceOutput.textContent = event.target.value + " "+name
 },500)
 
-debounceInput.addEventListener("input", handleDebounce)
+debounceInput.addEventListener("input", (event)=>{
+    handleDebounce(event, "mishra")
+})
 
 
 
-
-const throttleBtn = document.getElementById("throttleBtn");
 const throttleCount = document.getElementById("throttleCount");
-let count = 0;
+const throttleBtn = document.getElementById("throttleBtn");
 
 const throttle = (fn, limit) => {
     let lastExecution = 0;
 
-    return function(...args){
-        const now = Date.now();
-        if(now-lastExecution >= limit){
-            lastExecution = now;
-            fn.apply(this, args);
-        }
+    return function (...args){
+       const now = Date.now();
+       if(now-lastExecution >= limit){
+          lastExecution = now;
+          fn(...args);
+       }
     }
 }
 
-const handleThrottle = throttle(function(){
+let count = 0;
+const handleThrottle = throttle((str)=>{
     count++;
-    throttleCount.textContent = count;
+    throttleCount.textContent = str +" " + count
 },1000)
 
-throttleBtn.addEventListener("click", handleThrottle);
+throttleBtn.addEventListener("click", (event)=>{
+    handleThrottle("your count is")
+})
